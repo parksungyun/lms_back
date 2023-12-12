d1d35f1b7b3077466b657a9ce2bfcc997fc8f4cf
HanHwiyun <hyyun0430@gmail.com>
2023-12-12 16:36:33 +0900

    23-12-12

--- src/main/java/com/ac/yy/Repository/StudyRepository.java
+++ src/main/java/com/ac/yy/Repository/StudyRepository.java
@@ -2,8 +2,11 @@ package com.ac.yy.Repository;
 
 import com.ac.yy.Entity.StudyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +16,14 @@ public interface StudyRepository extends JpaRepository<StudyEntity, Integer> {
     Optional<StudyEntity> findByStudentIdAndLectureId(int studentId, int lectureId);
     List<StudyEntity> findByLectureId(int lectureId);
     List<StudyEntity> findByStudentId(int studentId);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE studies SET studies.progress_time=?2 WHERE studies.study_id=?1", nativeQuery = true)
+    int modifyingProgressTimebyStudyId(int id, int time);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE studies SET studies.is_study=?2 WHERE studies.study_id=?1", nativeQuery = true)
+    int modifyingIsStudybyStudyId(int id, int study);
 }
